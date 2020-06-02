1d8d5167b55a545a85574c197c3951221a040149
Adewale Orotayo <didymus7007@gmail.com>
2020-06-02 23:57:56 +0100

    Using Forms Form helpers such as form_tag and form_for used in building forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, { presence: true, length: { minimum: 2 }, uniqueness: true }
-  validates :email, { presence: true, uniqueness: true}
+  validates :email, { presence: true, uniqueness: true }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 end
